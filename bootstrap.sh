117478ed8c86acffe485c033ebe2f094e8982443
Bernhard Millauer <bernhard.millauer@gmail.com>
2013-04-18 01:24:47 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,7 @@ cd "$(dirname "${BASH_SOURCE}")"
 git pull
 function doIt() {
   rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" --exclude "apt-install.sh" -av . ~
+  . apt-install.sh
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
