59052d23f872a2cd72a93047b1649a81753db35d
SeriousM <masta2ooo@gmail.com>
2013-04-21 11:15:05 +0200

    Added heroku to the install packages; Added some echo messages to show what is happening; Silenced some commands; Added a sudo-block to avoid prepending every command with sudo;

--- apt-install.sh
+++ apt-install.sh
@@ -6,26 +6,60 @@ if [ "`whoami`" == "root" ]; then
 	exit 1
 fi
 
-# add the repo for sublime-text
-sudo add-apt-repository -y ppa:webupd8team/sublime-text-2
-sudo apt-get update
+echo "This script requires superuser access to install apt packages."
+echo "You will be prompted for your password by sudo."
 
-# build-essential for g++ compiling
-sudo apt-get install -y sublime-text build-essential terminator curl mongodb
+# clear any previous sudo permission
+sudo -k
+
+# run inside sudo
+sudo sh <<SCRIPT
+	# partly copied from https://toolbelt.heroku.com/install-ubuntu.sh
+
+	# add heroku repository to apt
+	echo "add heroku repository to apt"
+	apt-add-repository -y 'deb http://toolbelt.herokuapp.com/ubuntu ./' > /dev/null
+
+	# install heroku's release key for package verification
+	echo "add heroku key to apt"
+	wget -q -O- https://toolbelt.heroku.com/apt/release.key | apt-key add - > /dev/null
+
+	# add the repo for sublime-text
+	echo "add sublime repository to apt"
+	add-apt-repository -y ppa:webupd8team/sublime-text-2 > /dev/null 2>1&
+	
+	# update sources
+	echo "updating apt-get sources"
+	apt-get update > /dev/null
+
+	# build-essential is for g++ compiling
+	echo "start installing / updating packages"
+	apt-get install -y heroku git tig sublime-text build-essential terminator curl mongodb
+SCRIPT
 
 # install / upgrade rvm
-curl -L get.rvm.io | bash -s stable --auto
+echo "installing rvm"
+curl -s -L get.rvm.io | bash -s stable --auto
 . ~/.bash_profile
+rvm reload
 
-rvm autolibs enable
-rvm requirements
+echo "enable rvm autolibs"
+rvm autolibs enable > /dev/null
 
+echo "installing rvm requirements"
+rvm requirements > /dev/null
+
+echo "installing ruby 1.9.3"
 rvm install 1.9.3
+
+echo "setting up ruby 1.9.3 as default version to use"
 rvm use 1.9.3
 rvm --default use 1.9.3-p392
 rvm rvmrc warning ignore allGemfiles
 
+echo "installing bundler"
 gem install bundler
+echo "installing rails 3.2.9"
 gem install rails -v 3.2.9
 
-echo "Done"
+echo "We are done, thank you for using our services!"
