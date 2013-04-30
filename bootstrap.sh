bf89a37f93285c27bd402b95d3a0dfdc2b11ee58
SeriousM <masta2ooo@gmail.com>
2013-04-30 14:17:48 +0200

    Added the Windows Font Consolas;

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,10 @@ function doIt() {
   . sync-dotfiles.sh
   . apt-install.sh
   . auth-check.sh
+  
+  # renew the font cache
+  sudo fc-cache -f -v
+  
   echo "We are done, thank you for using our services!"
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
